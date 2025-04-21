8e0dba8a6c3c9e05fc0643085b7c70432081d78f
AndrewTaro <36262823+AndrewTaro@users.noreply.github.com>
2025-04-21 18:32:46 +0900

    Fix

--- PnFMods/Draggable/Main.py
+++ PnFMods/Draggable/Main.py
@@ -26,18 +26,18 @@ STOP_DRAGGING   = 'action.modStopDragging'
 
 class Draggable(object):
     def __init__(self):
-        self._stageWidth = None
-        self._stageHeight = None
-        self._stageScale = None
-        self._x = None
-        self._y = None
-        self._offsetX = None
-        self._offsetY = None
-        self._maxBoundsX = None
-        self._maxBoundsY = None
-        self._isDragging = None
+        self.stageWidth = None
+        self.stageHeight = None
+        self.stageScale = None
+        self.x = None
+        self.y = None
+        self.offsetX = None
+        self.offsetY = None
+        self.maxBoundsX = None
+        self.maxBoundsY = None
+        self.isDragging = None
         self.__stage = None
-        self._dragEntity = None
+        self.__dragEntity = None
 
         self._addEvents()
 
@@ -45,16 +45,16 @@ class Draggable(object):
         stageEntity = dataHub.getSingleEntity('stage')
         if stageEntity:
             stage = stageEntity[CC.stage]
-            stage.evStageSizeChanged.add(self.onStageChange)
+            stage.evStageSizeChanged.add(self.onStageUpdate)
             self.__stage = stage
-            self.onStageChange()
+            self.onStageUpdate()
             events.onSFMEvent(self.onSFMEvent)
         else:
             logError('Stage entity was not found.')
 
     def onSFMEvent(self, eventName, eventData):
         if eventName == START_DRAGGING:
-            if self._isDragging:
+            if self.isDragging:
                 logError('stop!')
                 self.onStopDragging()
             elementName = eventData.get('elementName', 'modDraggable')
@@ -63,7 +63,7 @@ class Draggable(object):
             self.onStartDragging(elementName, dragOffsets, elementSize)
 
         elif eventName == STOP_DRAGGING:
-            if not self._isDragging:
+            if not self.isDragging:
                 logError('it wasnt even started')
             else:
                 self.onStopDragging()
@@ -72,40 +72,39 @@ class Draggable(object):
         logInfo('Start dragging')
         dragEntity = EntityController(elementName)
         dragEntity.createEntity()
-        self._dragEntity = dragEntity
-        self._offsetX = dragOffsets[0]
-        self._offsetY = dragOffsets[1]
-        self._maxBoundsX = self._stageWidth  - elementSize[0]
-        self._maxBoundsY = self._stageHeight - elementSize[1]
+        self.__dragEntity = dragEntity
+        self.offsetX = dragOffsets[0]
+        self.offsetY = dragOffsets[1]
+        self.maxBoundsX = self.stageWidth  - elementSize[0]
+        self.maxBoundsY = self.stageHeight - elementSize[1]
         events.onMouseEvent(self.onMouseEvent)
-        self._isDragging = True
+        self.isDragging = True
 
     def onStopDragging(self, *args):
-        self._isDragging = False
+        self.isDragging = False
         events.cancel(self.onMouseEvent)
-        self._maxBoundsX = None
-        self._maxBoundsY = None
-        self._offsetX = None
-        self._offsetY = None
-        self._dragEntity.removeEntity()
-        self._dragEntity = None
+        self.maxBoundsX = None
+        self.maxBoundsY = None
+        self.offsetX = None
+        self.offsetY = None
+        self.__dragEntity.removeEntity()
+        self.__dragEntity = None
         logInfo('Stop dragging')
 
     def onMouseEvent(self, mouseEvent):
-        if self._isDragging:
+        if self.isDragging:
             relPos = mouseEvent.cursorPosition # guaranteed to be in screen
-            mouseX = (relPos[0] + 1) * 0.5 * self._stageWidth
-            mouseY = (1 - relPos[1]) * 0.5 * self._stageHeight
-            dragX = min(max(mouseX - self._offsetX, 0), self._maxBoundsX)
-            dragY = min(max(mouseY - self._offsetY, 0), self._maxBoundsY)
-            if self._dragEntity:
-                self._dragEntity.updateEntity({'dragPosX': dragX, 'dragPosY': dragY})
+            mouseX = (relPos[0] + 1) * 0.5 * self.stageWidth
+            mouseY = (1 - relPos[1]) * 0.5 * self.stageHeight
+            dragX = min(max(mouseX - self.offsetX, 0), self.maxBoundsX)
+            dragY = min(max(mouseY - self.offsetY, 0), self.maxBoundsY)
+            self.__dragEntity.updateEntity({'dragPosX': dragX, 'dragPosY': dragY})
 
-    def onStageChange(self, *args):
+    def onStageUpdate(self, *args):
         if self.__stage:
-            self._stageScale  = self.__stage.scale
-            self._stageWidth  = self.__stage.width
-            self._stageHeight = self.__stage.height
+            self.stageScale  = self.__stage.scale
+            self.stageWidth  = self.__stage.width
+            self.stageHeight = self.__stage.height
 
 
 draggable = Draggable()
