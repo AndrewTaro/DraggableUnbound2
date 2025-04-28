9659d1158cf4e81e4164e2ce446cfd361aabfb27
AndrewTaro <36262823+AndrewTaro@users.noreply.github.com>
2025-04-28 12:07:33 +0900

    Add

--- PnFMods/Draggable/Main.py
+++ PnFMods/Draggable/Main.py
@@ -9,6 +9,7 @@ except:
 from EntityController import EntityController
 
 CC = constants.UiComponents
+DEFAULT_POSITION = (100, 100)
 
 def logInfo(*args):
     data = [str(i) for i in args]
@@ -19,8 +20,11 @@ def logError(*args):
     utils.logError( '[{}] {}'.format(MOD_NAME, ', '.join(data)) )
 
 
-START_DRAGGING  = 'action.modStartDragging'
-STOP_DRAGGING   = 'action.modStopDragging'
+class DragEvents(object):
+    ADD_DRAGGABLE       = 'action.modAddDraggable'
+    REMOVE_DRAGGABLE    = 'action.modRemoveDraggable'
+    START_DRAGGING      = 'action.modStartDragging'
+    STOP_DRAGGING       = 'action.modStopDragging'
 
 
 class Rectangle(object):
@@ -60,17 +64,19 @@ class Rectangle(object):
 
 
 class Draggable(object):
-    def __init__(self):
-        self.stageWidth = None
-        self.stageHeight = None
-        self.stageScale = None
+    def __init__(self, elementName, defaultPosition):
+        self.elementName = elementName
         self.screenRect = None
         self.elementRect = None
         self.dragOffset = None
-        self._screenBoundsOffset = None
+        self.__screenBoundsOffset = None
         self.isDragging = None
         self.__stage = None
-        self.__dragEntity = None
+        self.defaultPosition = defaultPosition
+
+        dragEntity = EntityController(elementName)
+        dragEntity.createEntity(self.getSavedPosition())
+        self.__dragEntity = dragEntity        
 
         self._addEvents()
         logInfo('Initialized')
@@ -79,47 +85,46 @@ class Draggable(object):
         stageEntity = dataHub.getSingleEntity('stage')
         if stageEntity:
             stage = stageEntity[CC.stage]
-            stage.evStageSizeChanged.add(self.onStageSizeUpdate)
             self.__stage = stage
             self.onStageSizeUpdate()
-            events.onSFMEvent(self.onSFMEvent)
+            stage.evStageSizeChanged.add(self.onStageSizeUpdate)
         else:
             logError('Stage entity was not found.')
 
-    def onSFMEvent(self, eventName, eventData):
-        if eventName == START_DRAGGING:
-            if self.isDragging:
-                logError('Something is still being dragged!', self.__dragEntity._componentKey)
-                self.stopDragging()
-            elementName = eventData.get('elementName', None)
-            elementBounds = eventData.get('elementBounds', None)
-            elementPosition = eventData.get('elementPosition', None)
-            screenBoundsOffset = eventData.get('screenBoundsOffset', None)
-            dragOffset = eventData.get('dragOffset', None)
-            if elementName and elementPosition and elementBounds and dragOffset:
-                self.startDragging(elementName, elementPosition, elementBounds, screenBoundsOffset, dragOffset)
-            else:
-                logError('Draggable arguments are missing', elementName, elementBounds, elementPosition)
+    def getSavedPosition(self, *args):
+        x =  ui.getUserPrefs('chatBoxWidth', self.elementName + '_positionX', self.defaultPosition.get('positionX', DEFAULT_POSITION[0]))
+        y =  ui.getUserPrefs('chatBoxWidth', self.elementName + '_positionY', self.defaultPosition.get('positionY', DEFAULT_POSITION[1]))
+        return {'dragPosX': x, 'dragPosY': y}
 
-        elif eventName == STOP_DRAGGING:
-            if not self.isDragging:
-                logError('it wasnt even started')
-            else:
-                self.stopDragging()
+    def startDragging(self, data):
+        # Origin
+        #   ○--------------------
+        #   |                    |
+        #   |          Pivot     |
+        #   |  Offset    ○       |
+        #   |    ○               |
+        #    --------------------
+        #
+        if self.isDragging:
+            logError('It is still being dragged!', self.elementName)
+            self.stopDragging()
 
-    def startDragging(self, elementName, elementPosition, elementBounds, screenBoundsOffset, dragOffset):
-        logInfo('Start dragging')
-        dragEntity = EntityController(elementName)
-        dragEntity.createEntity()
-        self.__dragEntity = dragEntity
-        self.dragOffset = dragOffset
-        self._screenBoundsOffset = screenBoundsOffset
-        self.screenRect = self.__createScreenRect()
-        pivotX, pivotY = self.__calculatePivot(elementPosition, elementBounds)
-        self.elementRect = Rectangle(elementPosition[0], elementPosition[1], elementBounds[2], elementBounds[3], pivotX, pivotY)
+        elementBounds = data.get('elementBounds', None)
+        elementPosition = data.get('elementPosition', None) # Pivot Point
+        screenBoundsOffset = data.get('screenBoundsOffset', None)
+        dragOffset = data.get('dragOffset', None) # Offset to Origin
+        if elementPosition and elementBounds and dragOffset:
+            logInfo('Start dragging')
+            self.dragOffset = dragOffset
+            self.__screenBoundsOffset = screenBoundsOffset
+            self.screenRect = self.__createScreenRect()
+            pivotX, pivotY = self.__calculatePivot(elementPosition, elementBounds)
+            self.elementRect = Rectangle(elementPosition[0], elementPosition[1], elementBounds[2], elementBounds[3], pivotX, pivotY)
 
-        events.onMouseEvent(self.onMouseEvent)
-        self.isDragging = True
+            events.onMouseEvent(self.onMouseEvent)
+            self.isDragging = True
+        else:
+            logError('Draggable arguments are missing. name: {}, bounds: {}, pos: {}, offset'.format(self.elementName, elementBounds, elementPosition, dragOffset))
 
     def __calculatePivot(self, elementPosition, elementBounds):
         pivotX = (elementPosition[0] - elementBounds[0]) / elementBounds[2] # (pivotX - originX) / width
@@ -127,27 +132,38 @@ class Draggable(object):
         return (pivotX, pivotY)
 
     def __createScreenRect(self):
-        bounds = self._screenBoundsOffset
+        bounds = self.__screenBoundsOffset
         left    = bounds.get('left', 0)
         top     = bounds.get('top', 0)
         right   = bounds.get('right', 0)
         bottom  = bounds.get('bottom', 0)
-        stage   = self.__stage
-        w = stage.width - left - right
-        h = stage.height - top - bottom
+        w = self.stageWidth  - left - right
+        h = self.stageHeight - top - bottom
         return Rectangle(x=left, y=top, width=w, height=h)
 
     def stopDragging(self, *args):
+        if not self.isDragging:
+            logError('Dragging has not started')
+            return
+        
         self.isDragging = False
         events.cancel(self.onMouseEvent)
+        self.__dragEntity.updateEntity({'dragPosX': self.elementRect.x, 'dragPosY': self.elementRect.y, 'isDragging': False})
+        logInfo('Stop dragging')
 
-        self._screenBoundsOffset = None
-        self.screenRect = None
-        self.elementRect = None
-
+    def kill(self, *args):
+        if self.isDragging:
+            self.stopDragging()
+        self.__stage.evStageSizeChanged.remove(self.onStageSizeUpdate)
+        self.__stage = None
         self.__dragEntity.removeEntity()
         self.__dragEntity = None
-        logInfo('Stop dragging')
+        self.__screenBoundsOffset = None
+        self.screenRect = None
+        self.elementRect = None
+        self.dragOffset = None
+        self.defaultPosition = None
+        self.isDragging = None
 
     def onMouseEvent(self, mouseEvent):
         if self.isDragging:
@@ -157,14 +173,64 @@ class Draggable(object):
             element = self.elementRect
             element.x, element.y = mouseX - self.dragOffset[0], mouseY - self.dragOffset[1]
             element.clampWithin(self.screenRect)
-            self.__dragEntity.updateEntity({'dragPosX': element.x, 'dragPosY': element.y})
+            self.__dragEntity.updateEntity({'dragPosX': element.x, 'dragPosY': element.y, 'isDragging': True})
 
     def onStageSizeUpdate(self, *args):
-        self.stageWidth = self.__stage.width
-        self.stageHeight = self.__stage.height
-        self.stageScale = self.__stage.scale
-        if self.screenRect and self._screenBoundsOffset:
+        if self.screenRect and self.__screenBoundsOffset:
             self.screenRect = self.__createScreenRect()
 
+    @property
+    def stageWidth(self):
+        return self.__stage.width
+    
+    @property
+    def stageHeight(self):
+        return self.__stage.height
+    
+    @property
+    def stageScale(self):
+        return self.__stage.scale
 
-draggable = Draggable()
+
+
+class DraggableManager(object):
+    def __init__(self):
+        self.__draggableMap = {}
+        events.onSFMEvent(self.onSFMEvent)
+
+    def addDraggable(self, data):
+        elementName = data.get('elementName')
+        if elementName not in self.__draggableMap:
+            default = data.get('defaultPosition', DEFAULT_POSITION)
+            self.__draggableMap[elementName] = Draggable(elementName, default)
+            logInfo('Added draggable', elementName)
+
+    def removeDraggable(self, data):
+        elementName = data.get('elementName')
+        if elementName in self.__draggableMap:
+            d = self.__draggableMap.pop(elementName)
+            d.kill()
+            logInfo('Removed draggable', elementName)
+
+    def startDragging(self, data):
+        elementName = data.get('elementName')
+        if elementName in self.__draggableMap:
+            self.__draggableMap[elementName].startDragging(data)
+
+    def stopDragging(self, data):
+        elementName = data.get('elementName')
+        if elementName in self.__draggableMap:
+            self.__draggableMap[elementName].stopDragging(data)
+
+    def onSFMEvent(self, eventName, eventData):
+        if eventName == DragEvents.ADD_DRAGGABLE:
+            self.addDraggable(eventData)
+        elif eventName == DragEvents.REMOVE_DRAGGABLE:
+            self.removeDraggable(eventData)
+        elif eventName == DragEvents.START_DRAGGING:
+            self.startDragging(eventData)
+        elif eventName == DragEvents.STOP_DRAGGING:
+            self.stopDragging(eventData)
+
+
+gManager = DraggableManager()
